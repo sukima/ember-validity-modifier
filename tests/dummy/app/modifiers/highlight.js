bf9e2fb3763592678cf4d03f44e27bf479ebea43
Devin Weaver <suki@tritarget.org>
2022-01-31 16:54:06 -0500

    Add example code snippet to demo

---
+++ tests/dummy/app/modifiers/highlight.js
@@ -0,0 +1,13 @@
+import hljs from 'highlight.js';
+import { modifier } from 'ember-modifier';
+
+import javascript from 'highlight.js/lib/languages/javascript';
+import handlebars from 'highlight.js/lib/languages/handlebars';
+
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('handlebars', handlebars);
+
+export default modifier(function highlight(element, _, { lang }) {
+  if (lang) { element.classList.add(`language-${lang}`); }
+  hljs.highlightElement(element);
+});

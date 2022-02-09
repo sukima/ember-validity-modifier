b907a2801baa258ceffc822ba536ce0eee9d08cd
Devin Weaver <suki@tritarget.org>
2022-02-09 16:14:39 -0500

    Default events to setValidity events
    Prior to this change, this addon was unable to handle bubbled focusout
    events by default.
    
    This change just uses the default provided by setValidity.
    
    This should have been included in last bug fix release.

--- addon/modifiers/validity.js
+++ addon/modifiers/validity.js
@@ -7,11 +7,7 @@ import { waitForPromise as waiterCallback } from '@ember/test-waiters';
 export default modifier(function (
   element,
   validators,
-  {
-    validateTracked = [],
-    validateImmediately = false,
-    on = 'change,input,blur'
-  }
+  { validateTracked = [], validateImmediately = false, on }
 ) {
   // Ember passes in the params as a Proxy. Use Array.from to convert
   let teardown = setValidity(element, Array.from(validators), { on, waiterCallback });
